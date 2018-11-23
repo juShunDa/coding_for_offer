f2efeba82857aaaf8f8ffa4a3466141a356d4df7
juideal <juideal@aliyun.com>
2018-11-23 16:04:34 +0800

    Initial commit

---
+++ src/_02_FindDuplicate.java
@@ -0,0 +1,50 @@
+/**
+ * FindDuplicate
+ *
+ * @author juideal
+ * @date 2016/10/31
+ */
+public class _02_FindDuplicate {
+  /**
+   * 推荐的做法，通过交换元素，将值i保存到numbers[i]
+   * 在numbers[i]不和i相等时，如果numbers[i]和numbers[numbers[i]]相等就说明重复元素；
+   * 否则就交换这两个元素，这个过程相当于排序。举个例子，通过交换将2放入numbers[2]。
+
+   * @param num 待查找的数组
+   * @param length 数组的长度，其实就是numbers.length
+   * @param duplicate 用于保存重复数字，第一个被找到的重复数字存放在duplication[0]中
+   * @return 如果在数组中有重复元素
+   */
+  public static int duplication(int[] num) {
+    if (num == null || num.length <= 0) {
+      return -1;
+    }
+    for (int i = 0; i <= num.length - 1; i++) {
+      if (num[i] < 0 || num[i] >= num.length) {
+        return -1;
+      }
+    }
+
+    for (int i = 0; i < num.length; i++) {
+      while (num[i] != i) {
+        if (num[i] == num[num[i]]) {
+          return num[i];
+        }
+        swap(num, i, num[i]);
+      }
+    }
+    return -1;
+  }
+
+  private static void swap(int[] num, int p, int q) {
+    int temp = num[p];
+    num[p] = num[q];
+    num[q] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] data = {2, 3, 1, 0, 2, 5, 3};
+    System.out.println(duplication(data));
+  }
+}
+
---
+++ src/_02_Hello.java
@@ -0,0 +1,53 @@
+/**
+ * FindDuplicate
+ *
+ * @author juideal
+ * @date 2016/10/31
+ */
+public class _02_Hello {
+  /**
+   * 推荐的做法，通过交换元素，将值i保存到numbers[i]
+   * 在numbers[i]不和i相等时，如果numbers[i]和numbers[numbers[i]]相等就说明重复元素；
+   * 否则就交换这两个元素，这个过程相当于排序。举个例子，通过交换将2放入numbers[2]。
+
+   * @param num 待查找的数组
+   * @param length 数组的长度，其实就是numbers.length
+   * @param duplication 用于保存重复数字，第一个被找到的重复数字存放在duplication[0]中
+   * @return 如果在数组中有重复元素
+   */
+  public static boolean duplication(int[] num, int length, int[] duplication) {
+    if (num == null || length <= 0) {
+      return false;
+    }
+    for (int i = 0; i <= length - 1; i++) {
+      if (num[i] < 0 || num[i] >= length) {
+        return false;
+      }
+    }
+
+    for (int i = 0; i < num.length; i++) {
+      while (num[i] != i) {
+        if (num[i] == num[num[i]]) {
+          duplication[0] = num[i];
+          return true;
+        }
+        swap(num, i, num[i]);
+      }
+    }
+    return false;
+  }
+
+  private static void swap(int[] num, int p, int q) {//注意参数传递问题
+    int temp = num[p];
+    num[p] = num[q];
+    num[q] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] data = {2, 3, 1, 0, 2, 5, 3};
+    int[] duplication = new int[1];
+    System.out.println(duplication(data, data.length, duplication));
+    System.out.println(duplication[0]);
+  }
+}
+
