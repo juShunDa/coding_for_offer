c7ef3406a546b7c0e519a46fdb8aaf32a1e6e25c
juideal <juideal@aliyun.com>
2019-04-18 22:18:04 +0800

    添加了第十题

--- src/P09StacksSimulateQueue.java
+++ src/P09StacksSimulateQueue.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -24,5 +26,37 @@ public class P09StacksSimulateQueue {
       throw new Exception("queue is empty");
     }
     return out.pop();
+  }
+}
+
+
+class QueueSimulateStack<T> {
+  private Queue<T> queue1 = new LinkedList<>();
+  private Queue<T> queue2 = new LinkedList<>();
+
+  public void push (T data) {
+    if (!queue2.isEmpty()) {
+      queue2.offer(data);
+    } else {
+      queue1.offer(data);
+    }
+  }
+
+  public T pop () {
+    if (!queue2.isEmpty()) {
+      int size = queue2.size();
+      for (int i = 0; i < size - 1; i++) {
+        queue1.offer(queue2.poll());
+      }
+      return queue2.poll();
+    } else if (!queue1.isEmpty()) {
+      int size = queue1.size();
+      for (int i = 0; i < size - 1; i++) {
+        queue2.offer(queue1.poll());
+      }
+      return queue1.poll();
+    } else {
+      return null;
+    }
   }
 }
