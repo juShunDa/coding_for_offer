416be7297c09d6ece7de4ce73c029cb213330e1b
juideal <juideal@aliyun.com>
2019-09-20 10:44:53 +0800

    添加了第17题

--- src/coding_for_fun/P17Print1ToMaxNDigits.java
+++ src/coding_for_fun/P17Print1ToMaxNDigits.java
@@ -1,7 +1,5 @@
 package coding_for_fun;
 
-import sun.awt.X11.XWMHints;
-
 /**
  * @program: coding_for_offer
  * @description: 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数即 999。
@@ -9,23 +7,22 @@ import sun.awt.X11.XWMHints;
  * @create: 2019-08-15 23:09
  **/
 public class P17Print1ToMaxNDigits {
-  public void printOneToMaxNDigits (int n) {
+  public void printOneToMaxnDigits (int n) {
     if (n < 0) {
       return;
     }
     char[] number = new char[n];
-    printOneToMaxNDigits(number, 0);
-
+    printOneToMaxnDigits(number, 0);
   }
 
-  private void printOneToMaxNDigits (char[] number, int digit) {
+  private void printOneToMaxnDigits (char[] number, int digit) {
     if (digit == number.length) {
       printNumber(number);
       return;
     }
     for (int i = 0; i < 10; i++) {
       number[digit] = (char) (i + '0');
-      printOneToMaxNDigits(number, digit + 1);
+      printOneToMaxnDigits(number, digit + 1);
     }
 
   }
@@ -43,6 +40,6 @@ public class P17Print1ToMaxNDigits {
 
   public static void main (String[] args) {
     P17Print1ToMaxNDigits solution = new P17Print1ToMaxNDigits();
-    solution.printOneToMaxNDigits(2);
+    solution.printOneToMaxnDigits(2);
   }
 }
